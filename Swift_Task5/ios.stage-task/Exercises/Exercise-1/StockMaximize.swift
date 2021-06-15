84bbb1363b04c61ce96f0f46f8c300e2bba30392
Lazzatios <lseiilova@nu.edu.kz>
2021-06-15 15:00:20 +0600

    feat: maximize stock profit and uncipher secret word (resubmit)

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,57 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        var newArray: [Int] = []
+        var anotherArray: [Int] = []
+        var biggestProfit = -1000000
+        var biggestProfit2 = -1000000
+        var diff = 0
+        var diff2 = 0
+        
+        if prices.count < 1 {
+            return 0
+        }
+        
+        for i in 0..<prices.count {
+            
+            newArray.append(prices[i])
+            
+            var sum = 0
+            var sum2 = 0
+            
+            if i >= 0 && i < newArray.count {
+                
+                for j in 0..<newArray.count {
+                    
+                    diff = newArray.last! - newArray[j]
+                    
+                    if diff > 0 {
+                        sum += diff
+                    }
+                    
+                    if diff < 0 {
+                        
+                        anotherArray.append(newArray.last!)
+                        
+                        for x in 0..<anotherArray.count {
+                            
+                            diff2 = anotherArray.last! - anotherArray[x]
+                            
+                            if diff2 > biggestProfit2 {
+                                biggestProfit2 = diff2
+                            }
+                        }
+                        
+                        sum2 += diff2
+                        
+                    }
+                    if sum > biggestProfit && sum2 > biggestProfit2 {
+                        biggestProfit = sum + sum2
+                    }
+                }
+            }
+        }
+        return biggestProfit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,63 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+        var counter = 0
+        var substr = ""
+        var secretWord = ""
+        
+        var decryptedMessage = ""
+        var repetitions: Int?
+        
+        let letters = message.map { String($0) }
+
+        for i in letters {
+            
+            // MARK: find number of secret words
+            if let intLetter = Int(i) {
+                if counter >= 1 {
+                    substr += i
+                    continue
+                }
+                
+                repetitions = intLetter
+                
+                continue
+            }
+
+            // MARK: letters analysis
+            if i == "[" {
+                counter += 1
+                if counter > 1 {
+                    substr += i
+                }
+            } else if i == "]" {
+                counter -= 1
+                if counter == 0 {
+                    
+                    secretWord = decryptMessage(substr)
+                    substr = ""
+                    if var currentRepetition = repetitions {
+                        while currentRepetition > 0 {
+                            currentRepetition -= 1
+                            decryptedMessage += secretWord
+                        }
+                        repetitions = nil
+                    } else {
+                        decryptedMessage += secretWord
+                    }
+                } else {
+                    substr += "]"
+                }
+            } else {
+                if counter == 0 {
+                    decryptedMessage += i
+                } else {
+                    substr += i
+                }
+            }
+        }
+
+        return decryptedMessage
     }
 }
